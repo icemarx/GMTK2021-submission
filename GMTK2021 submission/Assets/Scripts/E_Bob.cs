dce5f82f96561683dcfa267214ce5a4d0f885927
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2021-06-12 22:19:15 +0200

    Implemented automatic shooting

--- GMTK2021 submission/Assets/Scripts/E_Bob.cs
+++ GMTK2021 submission/Assets/Scripts/E_Bob.cs
@@ -8,6 +8,11 @@ public class E_Bob : Enemy {
     private Transform player;
 
     [SerializeField]
+    private float mean_shoot_time = 3;
+    [SerializeField]
+    private float shoot_time_diviation = 1.5f;
+
+    [SerializeField]
     private GameObject turret;
 
     public AudioSource shootSFX;
@@ -16,12 +21,23 @@ public class E_Bob : Enemy {
     private void Start() {
         GM = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
         player = GM.player.transform;
+
+        StartCoroutine("AutoShoot");
     }
 
     private void Update() {
         turret.transform.rotation = Quaternion.LookRotation(Vector3.forward, player.position - transform.position) * Quaternion.Euler(0,0,90);
 
         if (Input.GetKeyDown(KeyCode.I)) {
+            Shoot();
+        }
+    }
+
+    IEnumerator AutoShoot() {
+        while(true) {
+            float t = mean_shoot_time + Random.Range(-shoot_time_diviation, shoot_time_diviation);
+            yield return new WaitForSeconds(t);
+
             Shoot();
         }
     }
