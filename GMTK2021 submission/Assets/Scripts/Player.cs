1c16fb3a179df7372b0b94fa21e56301b48e99ca
Matjaz Ciglic <matjaz.ciglic.jst@gmail.com>
2021-06-12 10:22:24 +0200

    Implemented basic player movements, fixed physics a bit

--- GMTK2021 submission/Assets/Scripts/Player.cs
+++ GMTK2021 submission/Assets/Scripts/Player.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : PC
-{
+public class Player : PC {
+    // physics
+    public float SPEED = 1;
+    private Rigidbody2D rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void FixedUpdate()
+    {
+        Movement();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Movement() {
+        Vector2 movement_dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movement_dir = movement_dir.normalized;
+
+        rb.velocity = movement_dir * SPEED;
     }
 }
